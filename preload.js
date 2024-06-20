b64428cf739a8bb03412a81376a9ec454954baa0
Souna <59102661+SounaVR@users.noreply.github.com>
2024-06-20 15:15:47 +0200

    onRefreshFiles api ipc handler added

--- preload.js
+++ preload.js
@@ -2,7 +2,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('api', {
   selectFolder: () => ipcRenderer.invoke('select-folder'),
-  getFiles: (folderPaths, categories, listSubfolders) => ipcRenderer.invoke('get-files', folderPaths, categories, listSubfolders),
+  getFiles: (folderPath, categories, listSubfolders) => ipcRenderer.invoke('get-files', folderPath, categories, listSubfolders),
   openFile: (filePath) => ipcRenderer.invoke('open-file', filePath),
   deleteFile: (filePath) => ipcRenderer.invoke('delete-file', filePath),
   openLocation: (filePath) => ipcRenderer.invoke('open-location', filePath),
@@ -12,8 +12,9 @@ contextBridge.exposeInMainWorld('api', {
   logFilePath: (filePath) => ipcRenderer.invoke('log-file-path', filePath),
   readRecentFiles: () => ipcRenderer.invoke('read-recent-files'),
   clearRecentFiles: () => ipcRenderer.invoke('clear-recent-files'),
-  watchFolder: (folderPaths) => ipcRenderer.invoke('watch-folder', folderPaths),
+  watchFolder: (folderPath) => ipcRenderer.invoke('watch-folder', folderPath),
   stopWatching: () => ipcRenderer.invoke('stop-watching'),
-  onRefreshFiles: (callback) => ipcRenderer.on('refresh-files', callback),
-  openRecycleBin: () => ipcRenderer.invoke('open-recycle-bin')
+  openRecycleBin: () => ipcRenderer.invoke('open-recycle-bin'),
+  walkSync: (dirs, listSubfolders, countFilesOnly) => ipcRenderer.invoke('walk-sync', dirs, listSubfolders, countFilesOnly),
+  onRefreshFiles: (callback) => ipcRenderer.on('refresh-files', callback) // Add this line
 });
